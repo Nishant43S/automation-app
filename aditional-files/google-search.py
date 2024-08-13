724393cdc124036b8f947796c6a02ba72af50eae
Nishant43S <165580472+Nishant43S@users.noreply.github.com>
2024-08-13 15:10:55 +0530

    Add files via upload

---
+++ aditional-files/google-search.py
@@ -0,0 +1,31 @@
+import requests
+import pywhatkit as kit
+
+def check_internet_connection():
+    try:
+        # Attempt to make an HTTP GET request to a reliable site
+        response = requests.get("https://www.google.com", timeout=5)
+        # Check if the request was successful (status code 200)
+        return response.status_code == 200
+    except requests.ConnectionError:
+        return False
+    except requests.Timeout:
+        return False
+    
+
+def search_yt(text_search)->str:
+    try:
+        kit.search(text_search)
+    except:
+        print("Something went wrong...")
+
+if __name__ == "__main__":
+    if check_internet_connection():
+        
+        Search = input("Search: ")
+        if Search == "":
+            print("Write something")
+        else:
+            search_yt(text_search=Search)
+    else:
+        print("No internet connection.")
